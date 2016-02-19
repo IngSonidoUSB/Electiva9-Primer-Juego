00eab14fb243c58cfb5247742b8d86c8113f9df9
angarce1994 <angarce1994@hotmail.com>
2016-02-19 16:04:06 -0500

    grupo 5
    listo script

--- Clase1/Assets/Nivel 5/Grupo_5.cs
+++ Clase1/Assets/Nivel 5/Grupo_5.cs
@@ -2,6 +2,11 @@
 using System.Collections;
 
 public class Grupo_5 : MonoBehaviour {
+    /*Nathalia Beltrán
+    Natalia Murcia
+    Angie Guerrero
+    Adrían García
+    */
 
 	// Use this for initialization
 	void Start () {
