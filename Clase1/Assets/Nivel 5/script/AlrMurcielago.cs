01052c4798a1c5c9f8f4605335104238f1def622
angarce1994 <nathaliabeltranb@MacBook-de-Nathalia-Beltran.local>
2016-03-02 20:58:09 -0500

    murcielago

---
+++ Clase1/Assets/Nivel 5/script/AlrMurcielago.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlrMurcielago : MonoBehaviour {
+
+	public Transform MurcielagoPrefab;
+
+	public float nextMurcielagoTime = 10f;
+	private float spawnRate = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (nextMurcielagoTime < Time.time)
+		{
+			SpawnMurcielago();
+			nextMurcielagoTime = Time.time + spawnRate;
+
+			spawnRate *= 5f;
+			spawnRate = Mathf.Clamp(spawnRate, 1f, 2f);
+		}
+	}
+
+
+	void SpawnMurcielago()
+	{
+		float addxPos = Random.Range(-2f, 2f);
+		Vector3 spawnPos = transform.position + new Vector3(addxPos, 0, 0);
+		Instantiate(MurcielagoPrefab, spawnPos, Quaternion.identity);
+	}
+
+
+}
+
---
+++ Clase1/Assets/Nivel 5/script/MovMurcielago.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovMurcielago : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float fallSpeed = 4 * Time.deltaTime;
+		transform.position += new Vector3(fallSpeed, 0, 0);
+
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "pared R")
+		{
+			Destroy(GameObject.FindWithTag("murcielago"));
+		}
+	}
+
+	void OnCollisionExit(Collision col)
+	{
+		if (col.gameObject.tag == "pared R")
+		{
+					}
+
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Mariposa")
+		{
+			//print ("Works");
+			Destroy(col.gameObject);
+		}
+}
+}
