810055e303fb05f1c85434237e01c7a8d117053f
camilohg <camiloh.galeano@hotmail.com>
2016-03-08 19:10:20 -0500

    avance

---
+++ Clase1/Assets/Nivel 2/Nivel 2/Grupo_2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grupo_2 : MonoBehaviour {
+
+	/*
+	 * Felipe Luque
+	 * Michael Bonilla Velàsquez
+	 * Felipe Saavedra
+	 * Camilo Galeano Rubiano
+	 */
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
