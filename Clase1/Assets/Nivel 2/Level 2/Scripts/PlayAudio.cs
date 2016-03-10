21fc55517c093d0fcee76c46d94453e7a5fad970
camilohg <camiloh.galeano@hotmail.com>
2016-03-10 07:14:48 -0500

    H

---
+++ Clase1/Assets/Nivel 2/Level 2/Scripts/PlayAudio.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayAudio : MonoBehaviour {
+
+	public AudioClip Cat;
+    public AudioClip Cat1;
+    private AudioSource Sound;
+
+	// Use this for initialization
+	void Start () {
+
+		Sound = GetComponent<AudioSource>();
+		Sound.clip = Cat;
+
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Mariposa") {
+			
+			Sound.Play ();
+
+        if (col.gameObject.tag == "Coin") 
+
+                Sound.Play();
+            }
+	}
+}
